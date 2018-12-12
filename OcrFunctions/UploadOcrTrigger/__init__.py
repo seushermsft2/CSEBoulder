9a37d4b568816f3f8a446b325b40090472918de0
Sean Usher <seusher@exchange.microsoft.com>
2018-12-12 10:51:22 -0700

    Revert "temporarily add a fake password"
    This reverts commit 32fb07b5533f69dcf6aff40566244de56e5f4c2a.

--- OcrFunctions/UploadOcrTrigger/__init__.py
+++ OcrFunctions/UploadOcrTrigger/__init__.py
@@ -10,7 +10,6 @@ def main(myblob: func.InputStream):
     """
     Main entry point for image OCR processing when new image is uploaded.
     """
-    password = "P@ssw0rd!"
     logging.info(f"Python blob trigger function processed blob \n"
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes")
