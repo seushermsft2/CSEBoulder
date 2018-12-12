64dd18edad740a8f3ee0488234f8f9386dedab05
Sean Usher <seusher@exchange.microsoft.com>
2018-12-12 14:42:36 -0700

    wrap the precheckin and official scripts to the Jenkins interface is a single script per-build

---
+++ build/official.sh
@@ -0,0 +1,11 @@
+chmod +x ./build/install.sh
+chmod +x ./build/validate.sh
+
+./build/install.sh
+./build/validate.sh
+
+chmod +x ./build/build_docker.sh
+chmod +x ./build/push_docker.sh
+
+./build/build_docker.sh
+./build/push_docker.sh
---
+++ build/precheckin.sh
@@ -0,0 +1,9 @@
+chmod +x ./build/install.sh
+chmod +x ./build/validate.sh
+
+./build/install.sh
+./build/validate.sh
+
+chmod +x ./build/build_docker.sh
+
+./build/build_docker.sh
